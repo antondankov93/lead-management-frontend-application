6f5107137d63e9bdb08c8f43a22aae7d96cbf2ce
antondankov93 <antondankov93@gmail.com>
2025-03-15 22:17:48 -0400

    feat: adjust eslint

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  eslint: {
+    dirs: ["src"],
+  },
   compiler: {
     styledComponents: true,
   },
